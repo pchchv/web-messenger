1a62eb09b1f0ac223ee3e84a8a77f0deb01c04a8
pchchv <ipchchv@gmail.com>
2022-10-08 11:38:14 +0300

    Implement app with FastAPI

---
+++ backend/app/core/config.py
@@ -0,0 +1,7 @@
+class Settings:
+    DEBUG: bool = False
+    PROJECT_NAME: str = "Web Messenger"
+    PROJECT_VERSION: str = "1.0.0"
+
+
+settings = Settings()
---
+++ backend/app/main.py
@@ -0,0 +1,7 @@
+from fastapi import FastAPI
+from core.config import settings
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION, debug=settings.DEBUG
+)
