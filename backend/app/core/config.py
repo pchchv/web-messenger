2d479130d7bb63ebb7aaf35db4f33ac5b6c57874
pchchv <ipchchv@gmail.com>
2022-10-08 12:42:23 +0300

    Add configs

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -4,6 +4,7 @@ from secrets import token_hex
 class Settings:
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = token_hex(32)
+    CSRF_SECRET_KEY: str = token_hex(32)
     JWT_TIME_LIVE: int = 10800
     DBURL: str = "sqlite:///sqlite.db"
     DEBUG: bool = False
--- backend/app/core/tools.py
+++ backend/app/core/tools.py
@@ -1,8 +1,12 @@
 from db.database import Database
 from core.config import settings
+from utils.helpers import Parser
+from api.wsmanager import RoomsManager
 from utils.crypt import Encoder, Decoder
 
 
+manager = RoomsManager()
 encoder = Encoder(settings.SECRET_KEY)
 decoder = Decoder(settings.SECRET_KEY)
 database = Database()
+parser = Parser()
