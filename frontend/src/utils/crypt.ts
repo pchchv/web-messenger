6a2cab54f2adcada293196ffe4781abf3cc6c6c9
pchchv <ipchchv@gmail.com>
2022-10-11 15:07:43 +0300

    Implement crypt type

---
+++ frontend/src/utils/crypt.ts
@@ -0,0 +1,31 @@
+import jwt_decode from "jwt-decode"
+import { getCookie } from "./helpers"
+import { MessageType } from "../components/room/roomTypes"
+
+export type Session = {
+    name: string
+    password: string
+    username: string
+    msg_key: string
+}
+
+const decodeMessages: Function = (
+    encryptedMessages: Array<MessageType>
+    ): Array<MessageType> => {
+        let CryptoJS = require("crypto-js")
+        const session: Session = jwt_decode(getCookie("session"))
+        encryptedMessages.forEach((message) => {
+            let bytes = CryptoJS.AES.decrypt(Object(message).Message, session.msg_key)
+            Object(message).Message = bytes.toString(CryptoJS.enc.Utf8)
+        })
+        return encryptedMessages
+    }
+    const encodeMessage: Function = (message: string): string => {
+        let CryptoJS = require("crypto-js")
+        const session: Session = jwt_decode(getCookie("session"))
+        let encrypted = CryptoJS.AES.encrypt(message, session.msg_key)
+        return encrypted.toString()
+    }
+
+
+export { decodeMessages, encodeMessage }
